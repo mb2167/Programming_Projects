b681cb6e6a3052357740e571d3b583ac744be599
mb2167 <michaeljbunn2001@gmail.com>
2026-06-12 17:58:15 +0100

    Updated noise to be based on SNR

--- communication_simulation/communication_simulation/main.py
+++ communication_simulation/communication_simulation/main.py
@@ -10,43 +10,46 @@ def gen_bits(seed, size):
     return rng.integers(0, 2, size=size)
 
 # Convert the bits into a wave signal
-def bit_to_signal(array):
-    return (array * 2) - 1
+def bit_to_signal(bits):
+    return (bits * 2) - 1
 
 
 # ------------------------------ TRANSMISSION ------------------------------ 
 
 # Add noise to the signal
-def add_noise(array):
-    mean = 0
-    std = 0.5
-    return (array + np.random.normal(mean, std, np.size(array)))
+def add_noise(signal):
+    snr_db = 1
+    P_signal = np.mean((signal*signal))
+    snr_linear = 10 ** (snr_db / 10)
+    P_noise = P_signal / snr_linear
+    noise_std = np.sqrt(P_noise)
+    return signal + np.random.normal(0, noise_std, np.size(signal))
 
 
 # ------------------------------ POST-TRANSMISSION PROCESSING ------------------------------ 
 # Convert the signal to bits
-def signal_to_bit(array):
-    return np.where(array < 0, 0, 1) # If array value < 0, return 0, otherwise 1
+def signal_to_bit(signal):
+    return np.where(signal < 0, 0, 1) # If array value < 0, return 0, otherwise 1
     
 # Calculate the error between the input bits and output bits
-def calc_error(array1, array2):
-    error = np.not_equal(array1, array2)
-    return error.sum()/np.size(array1)
+def calc_error(sent_bits, received_bits):
+    error = np.not_equal(sent_bits, received_bits)
+    return error.sum()/np.size(sent_bits)
 
 # ------------------------------ UTILITIES / DEBUGGING ------------------------------
 
 # Visualise the noisy signal
-def plot_signal(array):
-    plt.plot(array)
+def plot_signal(signal):
+    plt.plot(signal)
     plt.ylim(-3,3)
-    plt.xlim(0, np.size(array))
+    plt.xlim(0, np.size(signal))
     plt.axhline(linestyle = "--", color = "red")
     plt.show()
 
 # ------------------------------ MAIN ------------------------------
 
 def main():
-    size = 1000
+    size = 10000
     seed = 1 # Seed for randomness
     
     random_bits = gen_bits(seed, size)
